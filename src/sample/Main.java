dc02eedb39a5c181e2881f4968d7913f34183f4b
gniadoo <gniadek.jakub1@gmail.com>
2019-12-09 22:29:22 +0100

    images add

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -3,20 +3,30 @@ package sample;
 import javafx.application.Application;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.SnapshotParameters;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.image.PixelReader;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class Main extends Application  {
 
     Stage window;                   //główne okienko
-    Scene menu, rozgrywka, ranking;   //trzy opcje do wejścia z menu
+    Scene scene1, scene2, scene3;   //trzy opcje do wejścia z menu
+    Image title = new Image(getClass().getResourceAsStream("label.png"));
+    Image menu = new Image(getClass().getResourceAsStream("menu.png"));
+
+    Image image = new Image(String.valueOf(Main.class.getResource("back.jpg")));
+    BackgroundSize backgroundSize = new BackgroundSize(100, 100, true, true, true, false);
+    BackgroundImage backgroundImage = new BackgroundImage(image, BackgroundRepeat.REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER, backgroundSize);
+    Background background = new Background(backgroundImage);
 
     public static void main(String[] args){
         launch(args);
@@ -26,8 +36,11 @@ public class Main extends Application  {
     public void start(Stage primaryStage) throws Exception {
         window = primaryStage;
         window.setResizable(false);
+        //scene1.getStylesheets().add("style.css");
 
-        Label label1 = new Label("Menu glowne");
+        Label label1 = new Label();
+        label1.setGraphic(new ImageView(title));
+
         Button button1 = new Button("Nowa gra");
         Button button2 = new Button("Ranking");
         Button button3 = new Button("Exit");
@@ -36,6 +49,7 @@ public class Main extends Application  {
         VBox layout1 = new VBox(30);        //vbox na przyciski zeby były na sroko
         layout1.setAlignment(Pos.CENTER);
         layout1.getChildren().addAll(label1, button1, button2, button3);
+        layout1.setBackground(background);
 
         Canvas canvas = new Canvas(800,495);    //canvas do rysowania
         var context = canvas.getGraphicsContext2D();
@@ -50,18 +64,18 @@ public class Main extends Application  {
         VBox layout2 = new VBox(5);             //vbox na canvas i dolne menu w oknie do rozgrywki
         layout2.getChildren().add(getHBox1(canvas));
         layout2.getChildren().add(getHBox2(canvas));
-        layout2.setStyle("-fx-background-color: green");
+        layout2.setStyle("-fx-background-color: #2F3133");
 
 
-        menu = new Scene(layout1, 800, 530);
-        rozgrywka = new Scene(layout2, 800, 530);
+        scene1 = new Scene(layout1, 800, 530);
+        scene2 = new Scene(layout2, 800, 540);
 
-        button1.setOnAction(e -> {window.setScene(rozgrywka); /*context.clearRect(0, 0, 800, 495);*/});
+        button1.setOnAction(e -> {window.setScene(scene2); /*context.clearRect(0, 0, 800, 495);*/});
         //button2.setOnAction(e -> window.setScene(scene3));
         button3.setOnAction(e -> window.close());
 
 
-        window.setScene(menu);
+        window.setScene(scene1);
         window.setTitle("Draw that thing");
         window.show();
 
@@ -83,25 +97,26 @@ public class Main extends Application  {
 
         HBox hBox = new HBox();
         hBox.getChildren().add(canvas);
-        hBox.setStyle("-fx-background-color: #5fa9ff");
+        hBox.setStyle("-fx-background-color: #414F66");
 
         //SnapshotParameters params = new SnapshotParameters();        //
-        //params.setFill(Color.TRANSPARENT);                           //tu jest ta konwersja (nie działa) canvasa do img
+        //params.setFill(Color.TRANSPARENT);                           //tu jest ta konwersja (nie działa) canvasa do img co mówięś że overkill
         //Image snapshot = canvas.snapshot(params, null);              //
         //snapshot.getPixelReader().getArgb();                         //
         return hBox;
     }
     public HBox getHBox2(Canvas canvas){                //a to jest se menu dolne w okienku rozgrywki
         var context = canvas.getGraphicsContext2D();
-        Button button4 = new Button("Menu");
+        Button button4 = new Button();
+        button4.setGraphic(new ImageView(menu));
 
         HBox hBox = new HBox();
         hBox.setAlignment(Pos.CENTER);
-        button4.setOnAction(e-> {window.setScene(menu); context.clearRect(0, 0, 800, 495);});
+        button4.setOnAction(e-> {window.setScene(scene1); context.clearRect(0, 0, 800, 495);});
         //hBox.setSpacing(10);
         hBox.getChildren().add(button4);
         return hBox;
     }
 
-// no i plan prezentacji
+// no i plan prezentacji elooo
 }
